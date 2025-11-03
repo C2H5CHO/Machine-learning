8d9449176582ab5154141883d1d9ef0b75b57e51
C2H5CHO <2403692189@qq.com>
2025-11-03 11:01:44 +0800

    Create 03_DataPreprocessing.py

---
+++ 【ML 第一弹】/DAY02：【ML】KNN/KNN/03_DataPreprocessing.py
@@ -0,0 +1,16 @@
+# 1. 依赖包
+from sklearn.preprocessing import MinMaxScaler
+
+# 2. 输入数据
+X = [[90, 2, 10, 40], [60, 4, 15, 45], [75, 3, 13, 46]]
+
+# 3. 特征预处理
+def data_preprocessing(X_train):
+    ## 3.1 实例化模型
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    ## 3.2 归一化
+    X_train = scaler.fit_transform(X_train)
+    return X_train
+
+X_train = data_preprocessing(X)
+print(f"X_train: {X_train}")
