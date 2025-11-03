d8831ef1315f0b202b74e72f221238b193ad563b
C2H5CHO <2403692189@qq.com>
2025-11-04 01:03:29 +0800

    Update 05_iris.py

--- 【ML 第一弹】/DAY02：【ML】KNN/KNN/05_iris.py
+++ 【ML 第一弹】/DAY02：【ML】KNN/KNN/05_iris.py
@@ -23,7 +23,17 @@ def visualize_data_iris(data_iris):
     df_iris['label'] = data_iris.target
     print(f"df_iris: {df_iris.head()}")
     print(f"data_iris.feature_names: {data_iris.feature_names}")
-    sns.lmplot(x='sepal length (cm)', y='sepal width (cm)', hue='label', data=df_iris, fit_reg=False)
+
+    fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+    axes = axes.flatten() # 将axes从2x3的二维数组转换为1维数组
+    sns.scatterplot(x='sepal length (cm)', y='sepal width (cm)', hue='label', data=df_iris, ax=axes[0], palette='Set1')
+    sns.scatterplot(x='sepal length (cm)', y='petal length (cm)', hue='label', data=df_iris, ax=axes[1], palette='Set1')
+    sns.scatterplot(x='sepal length (cm)', y='petal width (cm)', hue='label', data=df_iris, ax=axes[2], palette='Set1')
+    sns.scatterplot(x='sepal width (cm)', y='petal length (cm)', hue='label', data=df_iris, ax=axes[3], palette='Set1')
+    sns.scatterplot(x='sepal width (cm)', y='petal width (cm)', hue='label', data=df_iris, ax=axes[4], palette='Set1')
+    sns.scatterplot(x='petal length (cm)', y='petal width (cm)', hue='label', data=df_iris, ax=axes[5], palette='Set1')
     plt.show()
 
-visualize_data_iris(data_iris)
+# visualize_data_iris(data_iris)
+
+# 4. 划分数据
